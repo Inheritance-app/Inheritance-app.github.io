327c8615d3303517ff6db477c1f6591dd1fe0a75
Inheritance <91532026+Inheritance-app@users.noreply.github.com>
2022-03-29 20:58:10 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// install sw 
-self.addEventListener('install', evt => {
-    console.log('service worker has been installed');
-});
-// activate sw 
-self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
-});
