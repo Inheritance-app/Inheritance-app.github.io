7fd35887eb528a6364a7da614c0dc43663ab75db
Inheritance <91532026+Inheritance-app@users.noreply.github.com>
2022-03-29 20:57:00 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then((reg) => console.log('SService worker registered', reg))
-        .catch((err) => console.warn('Error while registering service worker', err));
-}
